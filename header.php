064eb29ede3d8a9668b749306d4c372db67dd495
Claudio La Barbera <labarbera.claudio@gmail.com>
2017-01-14 16:16:19 +0100

    Disabled horizontal scroll

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
 	<title><?php bloginfo('name'); ?> - <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0">
+  <meta name="apple-mobile-web-app-capable" content="yes" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php wp_head(); ?>
 </head> 
